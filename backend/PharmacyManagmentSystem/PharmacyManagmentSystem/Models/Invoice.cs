5065484aa60b7b301f2be58ae342c31e6193d44a
Kanita Bajrami <kb31985@seeu.edu.mk>
2026-01-08 22:14:28 +0100

    invoice invoiceitems supplier

---
+++ backend/PharmacyManagmentSystem/PharmacyManagmentSystem/Models/Invoice.cs
@@ -0,0 +1,13 @@
+﻿namespace PharmacyManagmentSystem.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        //Foreign key
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<InvoiceItem> InvoiceItems { get; set; }
+    }
+}
