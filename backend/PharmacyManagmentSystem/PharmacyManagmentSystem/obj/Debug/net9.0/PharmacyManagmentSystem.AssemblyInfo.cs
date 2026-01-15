6e7ad21851a00e06f15ce7c5f390f7e871ac54de
Kanita Bajrami <kb31985@seeu.edu.mk>
2026-01-15 16:46:40 +0100

    Auth DTO

--- backend/PharmacyManagmentSystem/PharmacyManagmentSystem/obj/Debug/net9.0/PharmacyManagmentSystem.AssemblyInfo.cs
+++ backend/PharmacyManagmentSystem/PharmacyManagmentSystem/obj/Debug/net9.0/PharmacyManagmentSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmacyManagmentSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5cdc67b894c31f712f4773c47d7c4393d77250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ae97a79c22429d2f55534f6ce3b58fe645eb969")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmacyManagmentSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmacyManagmentSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
